75f2da8b7f243074ccc2534726e450de44695224
Lu,Hong-Ji <49175517+Hpshboss@users.noreply.github.com>
2024-08-12 15:06:29 +0800

    Create SearchInsertPosition.c
    No.35

---
+++ Easy/SearchInsertPosition.c
@@ -0,0 +1,25 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    if (nums[0] >= target) return 0;
+    if (nums[numsSize - 1] < target) return numsSize;
+
+    int lidx = 0, ridx = numsSize - 1;
+    int midx = (lidx + ridx) /2;
+
+    while ((ridx - lidx) > 1)
+    {
+        if (nums[midx] > target)
+        {
+            ridx = midx;
+        }
+        else if (nums[midx] < target)
+        {
+            lidx = midx;
+        }
+        else
+        {
+            return midx;
+        }
+        midx = (lidx + ridx) / 2;
+    }
+    return lidx + 1;
+}
