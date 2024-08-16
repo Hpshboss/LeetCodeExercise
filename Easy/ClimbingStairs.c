1e786266cc3696eea12eb206337bb46be70b1628
Lu,Hong-Ji <49175517+Hpshboss@users.noreply.github.com>
2024-08-16 17:30:37 +0800

    Update ClimbingStairs.c

--- Easy/ClimbingStairs.c
+++ Easy/ClimbingStairs.c
@@ -1,35 +1,12 @@
-unsigned long long int orderPolynomial(unsigned long long int input, unsigned long long int stop)              
-{                              
-    unsigned long long int x = input; 
-    unsigned long long int ret = 1;             
-    while (x > stop) 
-    { 
-        ret *= x;  
-        x -= 1;
-    };     
-    return ret;   
-}
-
 int climbStairs(int n) {
-    unsigned long long int ones = n % 2;
-    unsigned long long int twos = n >> 1;
+    int *stairs = malloc(n * sizeof(int));
+    *stairs = 1;
+    *(stairs+1) = 2;
 
-    unsigned long long int ret = 0;
-    while (ones < n + 1)
+    for (int i = 2; i < n; i++)
     {
-        if (ones > twos)
-        {
-            ret += orderPolynomial(ones + twos, ones) / orderPolynomial(twos, 0);
-        }
-        else 
-        {
-            ret += orderPolynomial(ones + twos, twos) / orderPolynomial(ones, 0);
-        }
-        
-        printf("1(%llu;%llu): %llu\n", ones, twos, orderPolynomial(ones + twos, 0));
-        ones+=2;
-        twos-=1;
+        *(stairs + i) = *(stairs + i -1) + *(stairs + i -2);
     }
-    
-    return ret;
+
+    return stairs[n-1];
 }
