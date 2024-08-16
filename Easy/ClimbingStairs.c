fec5b746427269f1af5879df4ac5c7c722e468eb
Lu,Hong-Ji <49175517+Hpshboss@users.noreply.github.com>
2024-08-16 08:50:11 +0800

    Update ClimbingStairs.c but also fail for n > 45

--- Easy/ClimbingStairs.c
+++ Easy/ClimbingStairs.c
@@ -1,5 +1,35 @@
-int climbStairs(int n) {
-    if (n == 0) return 1;
+unsigned long long int orderPolynomial(unsigned long long int input, unsigned long long int stop)              
+{                              
+    unsigned long long int x = input; 
+    unsigned long long int ret = 1;             
+    while (x > stop) 
+    { 
+        ret *= x;  
+        x -= 1;
+    };     
+    return ret;   
+}
 
-    return n > 1 ? climbStairs(n-1) + climbStairs(n-2) : climbStairs(n-1);
+int climbStairs(int n) {
+    unsigned long long int ones = n % 2;
+    unsigned long long int twos = n >> 1;
+
+    unsigned long long int ret = 0;
+    while (ones < n + 1)
+    {
+        if (ones > twos)
+        {
+            ret += orderPolynomial(ones + twos, ones) / orderPolynomial(twos, 0);
+        }
+        else 
+        {
+            ret += orderPolynomial(ones + twos, twos) / orderPolynomial(ones, 0);
+        }
+        
+        printf("1(%llu;%llu): %llu\n", ones, twos, orderPolynomial(ones + twos, 0));
+        ones+=2;
+        twos-=1;
+    }
+    
+    return ret;
 }
