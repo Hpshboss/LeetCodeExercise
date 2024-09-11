f203e78671b9f9848201f437d3b4aaff11d0cccf
Lu,Hong-Ji <49175517+Hpshboss@users.noreply.github.com>
2024-09-11 10:58:49 +0800

    Update ContainsDuplicate.c

--- Easy/ContainsDuplicate.c
+++ Easy/ContainsDuplicate.c
@@ -1,52 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- **/
+int cmp(const void* a, const void* b)
+{
+    return (*(int*)a - *(int*)b);
+}
 
-bool containsDuplicate(int* nums, int numsSize) {
-    struct TreeNode* head = malloc(sizeof(struct TreeNode));
-    struct TreeNode* sp = head;
+bool containsDuplicate(int* nums, int numsSize)
+{
+    qsort(nums, numsSize, sizeof(int), cmp);
 
-    head->val = nums[0]; head->left = NULL; head->right = NULL;
-    for (int i = 1; i < numsSize; i++)
+    for (int i = 0; i < numsSize - 1; i++)
     {
-        head = sp;
-        while (head != NULL)
-        {
-            if (head->val == nums[i]) return true;
-            if (head->val > nums[i])
-            {
-                if (head->left == NULL)
-                {
-                    struct TreeNode* node = malloc(sizeof(struct TreeNode));
-                    node->val = nums[i]; node->left = NULL; node->right = NULL;
-                    head->left = node;
-                    break;
-                }
-                else
-                {
-                    head = head->left;
-                }
-            }
-            else 
-            {
-                if (head->right == NULL)
-                {
-                    struct TreeNode* node = malloc(sizeof(struct TreeNode));
-                    node->val = nums[i]; node->left = NULL; node->right = NULL;
-                    head->right = node;
-                    break;
-                }
-                else
-                {
-                    head = head->right;
-                }
-            }
-        }
+        if (nums[i] == nums[i+1]) return true;
     }
+
     return false;
 }
