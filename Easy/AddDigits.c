42b83fb6fbb3bf531188f1b1d9ffd99b19493448
Lu,Hong-Ji <49175517+Hpshboss@users.noreply.github.com>
2024-09-27 10:43:44 +0800

    Create AddDigits.c
    No.258

---
+++ Easy/AddDigits.c
@@ -0,0 +1,14 @@
+int addDigits(int num) {
+    int sum = 0;
+    while(num >= 10)
+    {
+        sum = 0;
+        while (num != 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+        num = sum;
+    }
+    return num;
+}
