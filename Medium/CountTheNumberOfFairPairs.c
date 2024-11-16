2d18f51e936058033228a431b71df935a643cb84
Suricatalu <suricata.lu@gmail.com>
2024-11-16 08:51:14 +0800

    Update CountTheNumberOfFairPairs.c

--- Medium/CountTheNumberOfFairPairs.c
+++ Medium/CountTheNumberOfFairPairs.c
@@ -2,8 +2,22 @@ int compare(const void* a, const void* b) {
     return (*(int*)a - *(int*)b);
 }
 
+long long binSearch(int l, int r, int* nums, int target) {
+    int m = 0;
+
+    while (l <= r)
+    {
+        m = (l + r) >> 1;
+        if (nums[m] >= target)
+            r = m - 1;
+        else
+            l = m + 1;
+    }
+
+    return r;
+}
+
 long long countFairPairs(int* nums, int numsSize, int lower, int upper) {
-    long long singleret = 0;
     long long ret = 0;
 
     qsort(nums, numsSize, sizeof(int), compare);
@@ -12,36 +26,13 @@ long long countFairPairs(int* nums, int numsSize, int lower, int upper) {
     {
         int lidx = i + 1;
         int ridx = numsSize - 1;
-        printf("i=%d\n", i);
-        if (nums[i] + nums[lidx] > upper)
-            continue;
+        int low = lower - nums[i];
+        int up = upper - nums[i];
         
-        while (nums[i] + nums[lidx] < lower)
-        {
-            if (lidx == ridx)
-                break;
-            else
-                lidx++;
-        }
-
-        if (nums[i] + nums[ridx] < lower)
-            continue;
+        ridx = binSearch(lidx, ridx, nums, up + 1);
+        lidx = binSearch(lidx, ridx, nums, low);
         
-        while (nums[i] + nums[ridx] > upper)
-        {
-            if (lidx == ridx)
-                break;
-            else
-                ridx--;
-        }
-        if (ridx == lidx)
-        {
-            if (nums[i] + nums[ridx] > upper)
-                continue;
-            if (nums[i] + nums[ridx] < lower)
-                continue;
-        }
-        ret += (ridx - lidx + 1);
+        ret += (ridx - lidx);
     }
     return ret;
 }
