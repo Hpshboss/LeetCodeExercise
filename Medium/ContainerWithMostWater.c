012fa096659a2ac971658bcc82ab9a7a421975cf
Suricatalu <suricata.lu@gmail.com>
2025-01-03 11:00:35 +0800

    Update ContainerWithMostWater.c
    Algorithm:
    
    Designate two pointers ptr_lo = 0 and ptr_hi = length(height) - 1 i.e. set the two pointers at the two ends.
    If height[ptr_lo] <= height[ptr_hi] then increment ptr_lo.
    If height[ptr_lo] > height[ptr_hi] then decrement ptr_hi (i.e. iteratively move the smaller of the two edges closer to the other).
    Calculate the area after each change and keep track of the maximum area.
    End when the two pointers are equal to each other.
    
    Ref: labrador_peninsula

--- Medium/ContainerWithMostWater.c
+++ Medium/ContainerWithMostWater.c
@@ -1,26 +1,21 @@
 #define MINH(L, R) L < R ? L : R
 
 int maxArea(int* height, int heightSize) {
-    int width = heightSize - 1;
-    int rmaxh = 0; // relative max height for all
-    int rminh = 0; // two line height compare
-    int amaxh = 0; // absolute height
-    int rmaxa = 0; // relative max area
-    int amaxa = 0; // absolute max area
-    
-    for (; width > 0; width--)
+    int ret = 0;
+    int area = 0;
+    int lidx = 0;
+    int ridx = heightSize - 1;
+
+    while (ridx > lidx)
     {
-        for (int i = 0; i < heightSize - width; i++)
-        {
-            rminh = MINH(height[i], height[i+width]);
-            rmaxh = rminh > rmaxh ? rminh : rmaxh;
-        }
-        if (rmaxh > amaxh)
-        {
-            rmaxa = rmaxh * width;
-            amaxa = amaxa > rmaxa ? amaxa : rmaxa;
-        }
+        area = (ridx - lidx) * (MINH(height[lidx], height[ridx]));
+        ret = area > ret ? area : ret;
+
+        if (height[lidx] > height[ridx])
+            ridx--;
+        else
+            lidx++;
     }
 
-    return amaxa;
+    return ret;
 }
